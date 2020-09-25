24a7d1cfa2fb6b76fb06e819525aa520ff1b9ba1
Fakhruzy <fakhruzys@gmail.com>
2020-09-25 12:37:14 +0800

    with validation

--- Contacts_API.py
+++ Contacts_API.py
@@ -5,9 +5,30 @@ Created on Thu Sep 24 14:06:53 2020
 @author: Dell
 """
 
-from flask import Flask, request, abort, jsonify, make_response
+from flask import Flask, request, abort, jsonify
 from flask_pymongo import PyMongo
 from bson import ObjectId
+from marshmallow import Schema, fields, ValidationError
+from flask_request_validator import (Param, GET, validate_params)
+
+def validate_id(n):
+    contact = mongo.db.contact
+    if ObjectId.is_valid(n):
+            if contact.count_documents({ '_id': ObjectId(n) }, limit = 1) > 0:
+                return True
+            else:
+                abort(400, "Id does not exist")
+    else:
+        abort(400,"Id not valid")
+
+class PeopleSchema(Schema):
+    name = fields.String(required=True)
+    age = fields.Integer(required=True)
+    height = fields.Integer(required=True)
+
+class ContactsSchema(Schema):
+    email = fields.Email(required=True)
+    number = fields.String(required=True)
 
 app = Flask(__name__)
 
@@ -23,7 +44,8 @@ def index():
     return "Contact API for Air Asia Assesment!"
 
 @app.route('/contacts', methods=['GET'])
-def get_contacts():
+@validate_params(Param('q', GET, str, required=True))
+def get_contacts(query):
     query = request.args.get('q')
     contact = mongo.db.contact
 
@@ -54,6 +76,12 @@ def get_contacts():
 def add_person():
     if not request.json:
         abort(400)
+    schema = PeopleSchema()
+    try:
+        schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
     contact = mongo.db.contact
     person = {
             'name': request.json['name'],
@@ -61,16 +89,21 @@ def add_person():
          'height': request.json['height']
               }
     contact_id = contact.insert_one(person).inserted_id
-    #people.append(person)
     return "Inserted Successfully with the id {}".format(contact_id), 201
 
 @app.route('/people/<string:id>/contacts', methods=['POST'])
 def add_contacts(id):
     if not request.json:
         abort(400)
+    validate_id(id)
     contact = mongo.db.contact
+    schema = ContactsSchema()
+    try:
+        schema.load(request.json)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+    
     additional_contact = {
-           #'_id' : id,
          'email' : request.json['email'],
          'number': request.json['number']
                }
